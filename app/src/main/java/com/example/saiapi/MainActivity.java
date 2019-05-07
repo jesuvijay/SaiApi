0dc4562d6feb8bdf99a221b6a5963528c94bcfe6
Jesuraj <jesuvijayraj8246@gmail.com>
2019-05-07 11:04:48 +0530

    add local or online ip

--- app/src/main/java/com/example/saiapi/MainActivity.java
+++ app/src/main/java/com/example/saiapi/MainActivity.java
@@ -1,22 +1,22 @@
 package com.example.saiapi;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.saiapi.fragments.api.model.DeviceList;
 import com.example.saiapi.fragments.JwtFragment;
-import com.example.saiapi.fragments.ui.ApiClient;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private JwtFragment jwtFragment;
+    public static String BASE_URL = "http://52.14.32.86:8080/";
+    private String LocalURL = "http://192.168.0.202:8080/";
+    private String ONLINE_URL = "http://52.14.32.86:8080/";
 
 
     @Override
@@ -34,6 +34,32 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().replace(R.id.fragmentFrame, jwtFragment).commit();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.enableOnline:
+                if (!item.isChecked()) {
+                    // true
+                    BASE_URL = LocalURL;
+                    item.setChecked(true);
+                    Toast.makeText(this, "Local Ip enabled", Toast.LENGTH_SHORT).show();
+                } else {
+                    // false
+                    BASE_URL = ONLINE_URL;
+                    item.setChecked(false);
+                    Toast.makeText(this, "Local Ip disabled", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
 
+    }
 }
--- app/src/main/java/com/example/saiapi/fragments/ui/ApiClient.java
+++ app/src/main/java/com/example/saiapi/fragments/ui/ApiClient.java
@@ -7,6 +7,8 @@ import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static com.example.saiapi.MainActivity.BASE_URL;
+
 public final class ApiClient {
 
 
@@ -20,7 +22,7 @@ public final class ApiClient {
         OkHttpClient okHttpClient = new OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build();
 
 
-        String BASE_URL = "http://52.14.32.86:8080/";
+
         return new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .client(okHttpClient)
